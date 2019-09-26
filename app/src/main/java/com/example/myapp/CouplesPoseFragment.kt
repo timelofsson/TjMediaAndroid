3b7aeae2f13ff5395704a6e0260c3ebea8e8308f
Tim Tjärnskog <tim.tjarnskog@dizparc.se>
2019-09-26 15:50:36 +0200

    Add detail menu

--- app/src/main/java/com/example/myapp/CouplesPoseFragment.kt
+++ app/src/main/java/com/example/myapp/CouplesPoseFragment.kt
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapp.adapter.PoseAdapter
 import com.example.myapp.models.Pose
 
-class CouplesPoseFragment : Fragment(), PoseAdapter.OnPoseListener{
+class CouplesPoseFragment : Fragment() , PoseAdapter.OnPoseListener {
     private val poses: ArrayList<Pose> = ArrayList()
 
     override fun onCreateView(
@@ -21,11 +21,11 @@ class CouplesPoseFragment : Fragment(), PoseAdapter.OnPoseListener{
         savedInstanceState: Bundle?
     ): View? {
 
-        val rootView = inflater.inflate(R.layout.couple_poses_fragment, container, false)
+        val rootView = inflater.inflate(R.layout.other_fragment, container, false)
 
-        val recyclerView = rootView.findViewById<RecyclerView>(R.id.rv_main_list)
+        val recyclerView = rootView.findViewById<RecyclerView>(R.id.rv_detail_list)
         recyclerView.layoutManager = LinearLayoutManager(activity)
-        if(poses.size <= 0){
+        if (poses.size <= 0) {
             addPoses()
         }
         recyclerView.adapter = PoseAdapter(poses, this.context!!, this, resources)
@@ -44,16 +44,51 @@ class CouplesPoseFragment : Fragment(), PoseAdapter.OnPoseListener{
 
     // Adds animals to the empty animals ArrayList
     fun addPoses() {
-        poses.add(Pose(resources.getString(R.string.v_up_title), resources.getString(R.string.v_up_text), R.mipmap.v_up, R.drawable.v_up))
-        poses.add(Pose("Opened Up", resources.getString(R.string.opened_up_text), R.mipmap.opened_up, R.drawable.opened_up))
-        poses.add(Pose("Closed Up", resources.getString(R.string.closed_up_text), R.mipmap.closed_up, R.drawable.closed_up))
-        poses.add(Pose("Stacked", resources.getString(R.string.stacked), R.mipmap.stacked, R.drawable.couples_stacked))
-        poses.add(Pose("The Swing",  resources.getString(R.string.the_swing_text), R.mipmap.the_swing, R.drawable.the_swing))
-        poses.add(Pose("The Carry", resources.getString(R.string.the_carry_text), R.mipmap.the_carry, R.drawable.carry))
-        poses.add(Pose("Staggered Couple", resources.getString(R.string.the_staggered_couple_text), R.mipmap.staggered, R.drawable.staggered))
-        poses.add(Pose("Meet in the middle",  resources.getString(R.string.meet_in_middle_text), R.mipmap.meet_in_middle, R.drawable.meet_in_middle))
-        poses.add(Pose("The Walk", resources.getString(R.string.the_walk_text), R.mipmap.walk, R.drawable.the_walk))
-        poses.add(Pose("The Dip", resources.getString(R.string.the_dip_text), R.mipmap.dip, R.drawable.dip_pose))
 
+
+        poses.add(
+            Pose(
+                resources.getString(R.string.hipster_pose_title),
+                resources.getString(R.string.hipster_pose_text),
+                R.mipmap.hipster_pose,
+                intArrayOf(R.drawable.hipster_pose, R.drawable.hipster_pose_variation, R.drawable.hipster_pose_variation_2, R.drawable.opened_up)
+            )
+        )
+
+        poses.add(
+            Pose(
+                resources.getString(R.string.intimate_pose_title),
+                resources.getString(R.string.intimate_pose_text),
+                R.mipmap.intimate_pose,
+                intArrayOf(R.drawable.intimate, R.drawable.intimate_landscape)
+            )
+        )
+
+        poses.add(
+            Pose(
+                resources.getString(R.string.twirl_pose_title),
+                resources.getString(R.string.twirl_pose_text),
+                R.mipmap.the_twirl,
+                intArrayOf(R.drawable.the_twirl)
+            )
+        )
+
+        poses.add(
+            Pose(
+                resources.getString(R.string.waling_pose_title),
+                resources.getString(R.string.waling_pose_text),
+                R.mipmap.walk,
+                intArrayOf(R.drawable.the_walk, R.drawable.waling_shot)
+            )
+        )
+
+        poses.add(Pose(resources.getString(R.string.v_up_title), resources.getString(R.string.v_up_text), R.mipmap.v_up, intArrayOf(R.drawable.v_up)))
+        poses.add(Pose("Closed Up", resources.getString(R.string.closed_up_text), R.mipmap.closed_up, intArrayOf(R.drawable.closed_up)))
+        poses.add(Pose("Stacked", resources.getString(R.string.stacked), R.mipmap.stacked, intArrayOf(R.drawable.couples_stacked)))
+        poses.add(Pose("The Swing",  resources.getString(R.string.the_swing_text), R.mipmap.the_swing, intArrayOf(R.drawable.the_swing)))
+        poses.add(Pose("The Carry", resources.getString(R.string.the_carry_text), R.mipmap.the_carry, intArrayOf(R.drawable.carry)))
+        poses.add(Pose("Staggered Couple", resources.getString(R.string.the_staggered_couple_text), R.mipmap.staggered, intArrayOf(R.drawable.staggered)))
+        poses.add(Pose("Meet in the middle",  resources.getString(R.string.meet_in_middle_text), R.mipmap.meet_in_middle, intArrayOf(R.drawable.meet_in_middle)))
+        poses.add(Pose("The Dip", resources.getString(R.string.the_dip_text), R.mipmap.dip, intArrayOf(R.drawable.dip_pose)))
     }
 }
--- app/src/main/java/com/example/myapp/adapter/PoseAdapter.kt
+++ app/src/main/java/com/example/myapp/adapter/PoseAdapter.kt
@@ -12,7 +12,10 @@ import com.example.myapp.service.BitMapService
 import kotlinx.android.synthetic.main.pose_list_item.view.*
 
 
-class PoseAdapter(val items : ArrayList<Pose>, val context: Context, val mOnPoseListener: OnPoseListener, val resources : Resources) : RecyclerView.Adapter<PoseAdapter.ViewHolder> () {
+
+
+class PoseAdapter(val items : ArrayList<Pose>, val context: Context, val mOnPoseListener: OnPoseListener, val resources : Resources)
+    : RecyclerView.Adapter<PoseAdapter.ViewHolder> () {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.pose_list_item, parent, false), mOnPoseListener)
@@ -23,7 +26,7 @@ class PoseAdapter(val items : ArrayList<Pose>, val context: Context, val mOnPose
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvText?.text = items[position].title
+        holder.tvText.text = items[position].title
         holder.tvPictureUrl.setImageBitmap(BitMapService.drawableToBitmap(resources.getDrawable(items[position].image, context.theme)))
     }
 
